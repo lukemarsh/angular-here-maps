4596926dde27d37c5994876389b017937e8238a2
Luke Marsh <luke@theappbusiness.com>
2015-04-09 15:41:16 +0100

    adding bump to the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,25 @@ module.exports = function (grunt) {
     // Project settings
     yeoman: appConfig,
 
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json', 'bower.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'upstream',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
+    },
+
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       js: {
