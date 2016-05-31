97070d2e975cd24a2df229911115f45d159ae171
Massimo Rangoni <maxappbr@gmail.com>
2016-05-31 16:47:49 -0300

    Update unit tests and add some new tests

--- test/spec/services/MapConfig.js
+++ test/spec/services/MapConfig.js
@@ -4,7 +4,7 @@ describe('Service: MapConfig', function () {
 
   beforeEach(module('angular-here-maps'));
 
-  var MapConfigProvider;
+  var MapConfigProvider, MapConfig;
 
   beforeEach(function() {
     angular.module('mockModule', [])
@@ -12,11 +12,14 @@ describe('Service: MapConfig', function () {
         MapConfigProvider = _MapConfigProvider_;
 
         MapConfigProvider.setOptions({
-          appId: 'Q4azLpJlFAp5pkcEbagu',
-          appCode: 'WT6i13vXvx1JbFky92wqjg',
+          appId: 'dEuIUI8oyzMGgrJVq4dz',
+          appCode: '_g3jrZwSCHmKTRFcrySZQw',
           libraries: 'ui,mapevents,pano',
           pixelRatio: 2,
-          pixelPerInch: 320
+          pixelPerInch: 320,
+          useHTTPS: false,
+          zoomMax: 15,
+          zoomMin: 5
         });
       });
 
@@ -30,11 +33,11 @@ describe('Service: MapConfig', function () {
   });
 
   it('appId() should return the appId provided', function () {
-    expect(MapConfigProvider.mapOptions.appId).toEqual('Q4azLpJlFAp5pkcEbagu');
+    expect(MapConfigProvider.mapOptions.appId).toEqual('dEuIUI8oyzMGgrJVq4dz');
   });
 
   it('appCode() should return the appCode provided', function () {
-    expect(MapConfigProvider.mapOptions.appCode).toEqual('WT6i13vXvx1JbFky92wqjg');
+    expect(MapConfigProvider.mapOptions.appCode).toEqual('_g3jrZwSCHmKTRFcrySZQw');
   });
 
   it('libraries() should return the libraries provided', function () {
@@ -48,5 +51,55 @@ describe('Service: MapConfig', function () {
   it('pixel per inch should return the pixel per inch provided', function() {
     expect(MapConfigProvider.mapOptions.pixelPerInch).toEqual(320);
   });
+
+  it('zoomMax should return the zoomMax provided', function() {
+    expect(MapConfigProvider.mapOptions.zoomMax).toEqual(15);
+  });
+
+  it('zoomMin should return the zoomMin provided', function() {
+    expect(MapConfigProvider.mapOptions.zoomMin).toEqual(5);
+  });
+
+  it('useHTTPS should return the useHTTPS provided', function() {
+    expect(MapConfigProvider.mapOptions.useHTTPS).toEqual(false);
+  });
+
+  it('setOptions should update mapOptions', function() {
+    var fakeOptions = {
+      test: true
+    };
+    MapConfigProvider.setOptions(fakeOptions);
+    expect(MapConfigProvider.mapOptions).toEqual(fakeOptions);
+  });
+
+
+  describe('MapConfig default values', function() {
+
+    beforeEach(function(){
+
+      MapConfigProvider.setOptions({
+        appId: 'dEuIUI8oyzMGgrJVq4dz',
+        appCode: '_g3jrZwSCHmKTRFcrySZQw',
+        libraries: 'ui,mapevents,pano',
+        pixelRatio: 2,
+        pixelPerInch: 320
+      });
+
+      inject(function(_MapConfig_) {
+        MapConfig = _MapConfig_;
+      });
+
+    });
+
+    it('MapConfig.zoomMax() should return the default value 18', function() {
+      expect(MapConfig.zoomMax()).toEqual(18);
+    });
+
+    it('MapConfig.zoomMin() should return the default value 1', function() {
+      expect(MapConfig.zoomMin()).toEqual(1);
+    });
+
+  });
+
 
 });
